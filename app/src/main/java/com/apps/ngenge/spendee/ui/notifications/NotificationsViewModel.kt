2bc5c2374065130a6aa9291618375c27af426087
ngengesenior <seniorngenge@gmail.com>
2019-07-22 07:36:39 +0100

    Initial commit

---
+++ app/src/main/java/com/apps/ngenge/spendee/ui/notifications/NotificationsViewModel.kt
@@ -0,0 +1,13 @@
+package com.apps.ngenge.spendee.ui.notifications
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+class NotificationsViewModel : ViewModel() {
+
+    private val _text = MutableLiveData<String>().apply {
+        value = "This is notifications Fragment"
+    }
+    val text: LiveData<String> = _text
+}
